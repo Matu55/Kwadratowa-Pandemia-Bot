42a135553fc4c180a3418bc67adcfbcd57ae0946
Matu55 <MJedl@tutanota.com>
2024-06-27 18:10:48 +0200

    Version 1.2.9
    changed code formatting

--- commands.js
+++ commands.js
@@ -7,84 +7,49 @@ const botToken = process.env['token'];
 const rest = new Discord.REST({ version: '10' }).setToken(botToken);
 const slashRegister = async () => {
     try {
-        console.log("Próbowanie utworzenia komend!");
+        console.log('Próbowanie utworzenia komend!');
 
         await rest.put(Discord.Routes.applicationGuildCommands(botID, serverID), {
             body: [
                 new Discord.SlashCommandBuilder()
-                .setName("SendAnnounce")
-                .setDescription("Wyślij wiadomość na kanał z ogłoszeniami")
-                .addStringOption(option => {
-                    return option
-                    .setName("AnnounceText")
-                    .setDescription("Wpisz wiadomość która zostanie wysłana na kanał z ogłoszeniami")
-                    .setRequired(true)
-                }),
+                .setName('SendAnnounce')
+                .setDescription('Wyślij wiadomość na kanał z ogłoszeniami')
+                .addStringOption((option) => option.setName('AnnounceText').setDescription('Wpisz wiadomość która zostanie wysłana na kanał z ogłoszeniami').setRequired(true)),
 
                 new Discord.SlashCommandBuilder()
-                .setName("SendInfo")
-                .setDescription("Wyślij wiadomość na kanał z informacjami")
-                .addStringOption(option => {
-                    return option
-                    .setName("InfoText")
-                    .setDescription("Wpisz wiadomość która zostanie wysłana na kanał z informacjami")
-                    .setRequired(true)
-                }),
+                .setName('SendInfo')
+                .setDescription('Wyślij wiadomość na kanał z informacjami')
+                .addStringOption((option) => option.setName('InfoText').setDescription('Wpisz wiadomość która zostanie wysłana na kanał z informacjami').setRequired(true)),
 
                 new Discord.SlashCommandBuilder()
-                .setName("SendChangelog")
-                .setDescription("Wyślij wiadomość na kanał z changelogiem")
-                .addStringOption(option => {
-                    return option
-                    .setName("changelogText")
-                    .setDescription("Wpisz wiadomość która zostanie wysłana na kanał z changelogiem")
-                    .setRequired(true)
-                }),
+                .setName('SendChangelog')
+                .setDescription('Wyślij wiadomość na kanał z changelogiem')
+                .addStringOption((option) => option.setName('changelogText').setDescription('Wpisz wiadomość która zostanie wysłana na kanał z changelogiem').setRequired(true)),
                 
                 new Discord.SlashCommandBuilder()
-                .setName("SendBan")
-                .setDescription("Wyślij wiadomość na kanał z banami")
-                .addStringOption(option => {
-                    return option
-                    .setName("BanText")
-                    .setDescription("Wpisz wiadomość która zostanie wysłana na kanał z banami")
-                    .setRequired(true)
-                }),
+                .setName('SendBan')
+                .setDescription('Wyślij wiadomość na kanał z banami')
+                .addStringOption((option) => option.setName('BanText').setDescription('Wpisz wiadomość która zostanie wysłana na kanał z banami').setRequired(true)),
 
                 new Discord.SlashCommandBuilder()
-                .setName("SendUnban")
-                .setDescription("Wyślij wiadomość na kanał z unbanami")
-                .addStringOption(option => {
-                    return option
-                    .setName("UnBanText")
-                    .setDescription("Wpisz wiadomość która zostanie wysłana na kanał z unbanami")
-                    .setRequired(true)
-                }),
+                .setName('SendUnban')
+                .setDescription('Wyślij wiadomość na kanał z unbanami')
+                .addStringOption((option) => option.setName('UnBanText').setDescription('Wpisz wiadomość która zostanie wysłana na kanał z unbanami').setRequired(true)),
 
                 new Discord.SlashCommandBuilder()
-                .setName("SendEvent")
-                .setDescription("Wyślij wiadomość na kanał z eventami")
-                .addStringOption(option => {
-                    return option
-                    .setName("EventText")
-                    .setDescription("Wpisz wiadomość która zostanie wysłana na kanał z eventami")
-                    .setRequired(true)
-                }),
+                .setName('SendEvent')
+                .setDescription('Wyślij wiadomość na kanał z eventami')
+                .addStringOption((option) => option.setName('EventText').setDescription('Wpisz wiadomość która zostanie wysłana na kanał z eventami').setRequired(true)),
 
                 new Discord.SlashCommandBuilder()
-                .setName("SendWhitelist")
-                .setDescription("Wyślij wiadomość na kanał whitelist")
-                .addStringOption(option => {
-                    return option
-                    .setName("WhitelistText")
-                    .setDescription("Wpisz wiadomość która zostanie wysłana na kanał z whitelist")
-                    .setRequired(true)
-                }),
+                .setName('SendWhitelist')
+                .setDescription('Wyślij wiadomość na kanał whitelist')
+                .addStringOption((option) => option.setName('WhitelistText').setDescription('Wpisz wiadomość która zostanie wysłana na kanał z whitelist').setRequired(true)),
             ],
         });
-        console.log("Komendy zostały utworzone!");
+        console.log('Komendy zostały utworzone!');
     } catch (error) {
-        console.log("Wystąpił błąd podczas tworzenia komend!");
+        console.log('Wystąpił błąd podczas tworzenia komend!');
         console.error(error)
     }
 }
